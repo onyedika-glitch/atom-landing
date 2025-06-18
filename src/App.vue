bc365798309a4eba9715bae071c4e40a0e3e98f7
Omogo Peter <omogopeter48@gmail.com>
2025-06-18 16:01:33 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
-  <div class="relative">
+  <div class="relative w-full overflow-x-hidden">
     <Navbar />
     <router-view />
-    <About />
-    <Services />
-    <Footprint />
-    <Products />
-    <AI/> 
-    <Group/>
-    <How/>
-    <Contact/>
     <Footer/>
   </div>
 </template>
 
 <script lang="ts" setup>
 import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
-import About from '@/components/About.vue'
-import Services from '@/components/Services.vue'
-import Footprint from '@/components/Footprint.vue'
-import Products from '@/components/Products.vue'
-import AI from '@/components/AI.vue'
-import Group from '@/components/Group.vue'
-import How from '@/components/How.vue'
-import Contact from '@/components/Contact.vue'
-import Footer from '@/components/Footer.vue'
+import Footer from "@/components/Footer.vue";
 </script>
 
 <style>
-
+html, body {
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+  overflow-x: hidden;
+}
+ 
+#app {
+  width: 100%;
+  overflow-x: hidden;
+}
 </style>
