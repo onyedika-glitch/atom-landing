16f5283f1368b8f2a2a1db14f6b0a5fc50e6614c
Omogo Peter <omogopeter48@gmail.com>
2025-06-19 22:09:13 +0100

    Add files via upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 import About from '../components/About.vue';
 import Services from '../components/Services.vue';
-import Contact from '../components/Contact-us.vue';
+import ContactUs from '../components/Contact-us.vue';
+import Products from '@/components/Products.vue'
+import Contact from '@/components/Contact.vue'
+
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/about', name: 'About', component: About },
     { path: '/services', name: 'Services', component: Services },
-    { path: '/contact-us', name: 'Contact-us', component: Contact },
+    { path: '/contact-us', name: 'Contact-us', component: ContactUs },
+    { path: '/products', component: Products },
+    { path: '/contact', component: Contact },
 ];
 
 const router = createRouter({
